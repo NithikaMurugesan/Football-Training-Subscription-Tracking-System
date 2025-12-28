0e61b041544996015650c0628f039f02fb9fd07a
NithikaMurugesan <nithikamurugesan2896@gmail.com>
2025-12-28 23:46:12 +0530

    Add files via upload

--- springapp/src/main/java/com/examly/springapp/exception/GlobalExceptionHandler.java
+++ springapp/src/main/java/com/examly/springapp/exception/GlobalExceptionHandler.java
@@ -1,5 +1,27 @@
 package com.examly.springapp.exception;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler({
+        AttendanceNotFoundException.class,
+        MemberNotFoundException.class,
+        MembershipNotFoundException.class,
+        MembershipTypeNotFoundException.class,
+        PaymentNotFoundException.class
+    }) 
+    public ResponseEntity<String> handleException(RuntimeException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleGlobalException(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
 }
